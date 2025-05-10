71429630df1102781e45df8af3b200f93b347c42
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-09 23:03:41 -0700

    “..”

--- _typewriter/typewriter.js
+++
@@ -1,9 +0,0 @@
-const sentence = "hello there from lighthouse labs";
-
-for (const char of sentence) {
-	process.stdout.write(char);
-}
-
-setTimeout(() => {
-	// print the char here
-}, 1000); // <= 1s delay to make it noticeable. Can dial it down later.
